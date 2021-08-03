f7aa71e0e02445aa54f8610da767776b7d12c988
Adi Jo <adi.joost@gmail.com>
2021-08-03 12:46:23 +0200

    fixed: CORS now only allowed by trusted domains, before was allowed by anybody

--- resources/kid.py
+++ resources/kid.py
@@ -20,7 +20,7 @@ class KidsByDay(Resource):
         for kid in kids:
             return_value[kid.id] = kid.to_json()
         response = flask.jsonify(return_value)
-        response.headers.add('Access-Control-Allow-Origin', 'http://127..0.1:5000')
+        response.headers.add('Access-Control-Allow-Origin', 'http://127.0.0.1:5000')
         return response
         
 
@@ -31,7 +31,7 @@ class Kids(Resource):
         for kid in kids:
             return_value[kid.id] = kid.to_json()
         response = flask.jsonify(return_value)
-        response.headers.add('Access-Control-Allow-Origin', 'http://127..0.1:5000')
+        response.headers.add('Access-Control-Allow-Origin', 'http://127.0.0.1:5000')
         return response
 
 class Kid(Resource):
