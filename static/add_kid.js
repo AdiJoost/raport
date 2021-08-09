148f85804b89603360cf750b0848da5e0cbac3dc
Adi Joost <adi.joost@gmail.com>
2021-08-09 21:22:18 +0200

    add_kid.html now displays presents of kid with Checkboxes

--- static/add_kid.js
+++ static/add_kid.js
@@ -2,6 +2,12 @@
 
 let baseUrl = "";
 
+window.addEventListener("load", function(){
+	baseUrl = window.location.origin;
+	navSetup();
+	setupButton();
+}, false)
+
 function setupButton(){
 	let myButton = document.getElementById('btnAddKid')
 	myButton.addEventListener("click", function(){
@@ -15,13 +21,43 @@ function getBody(){
 
 	let myBody = {"name" : getValueByID("nameInput"),
 				"parents": getValueByID("parentsInput"),
-				"present": getValueByID("presentsInput"),
+				"present": getPresents(),
 				"birthday": getValueByID("birthdayInput"),
 				"eating": getValueByID("eatingInput"),
 				"sleeping": getValueByID("sleepingInput"),
 				"spez": getValueByID("spezInput"),
 				"important": getValueByID("importantInput")}
+	console.log(myBody);
 	return JSON.stringify(myBody);
+}
+
+function getPresents(){
+	let returnCode = "";
+	returnCode += getDayCode("Montag");
+	returnCode += getDayCode("Dienstag");
+	returnCode += getDayCode("Mittwoch");
+	returnCode += getDayCode("Donnerstag");
+	returnCode += getDayCode("Freitag");
+	return returnCode;
+}
+
+function getDayCode(day){
+	let morning = document.getElementById('morning' + day);
+	let lunch = document.getElementById('lunch' + day);
+	let afternoon = document.getElementById('afternoon' + day);
+
+	if (morning.checked && lunch.checked && afternoon.checked){
+		return "a";
+	} else if (morning.checked && lunch.checked && !afternoon.checked){
+		return "b";
+	} else if (morning.checked && !lunch.checked && !afternoon.checked){
+		return "c";
+	} else if (!morning.checked && lunch.checked && afternoon.checked){
+		return "d";
+	} else if (!morning.checked && !lunch.checked && afternoon.checked){
+		return "e";
+	} 
+	return "f";
 }
 
 function sendPost(payload){
@@ -78,8 +114,3 @@ function createNav(name, myUrlTail){
 }
 
 
-window.addEventListener("load", function(){
-	baseUrl = window.location.origin;
-	navSetup();
-	setupButton();
-}, false)
